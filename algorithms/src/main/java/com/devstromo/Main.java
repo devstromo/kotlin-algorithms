71eaa189e51a633130a5411e3c48548211e55a82
alemoles <alemolerio@gmail.com>
2024-01-31 20:20:27 -0300

    Kotlin Algorithms | Create algorithms data structure module

--- algorithms/src/main/java/com/devstromo/Main.java
+++
@@ -1,7 +0,0 @@
-package com.devstromo;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ data-structure/src/main/kotlin/Main.kt
@@ -0,0 +1,5 @@
+package com.devstromo
+
+fun main() {
+    println("Hello World!")
+}
