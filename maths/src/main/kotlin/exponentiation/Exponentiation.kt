d07ddf58f73ac25cc81940a026781919117024f1
alemoles <alemolerio@gmail.com>
2024-07-09 22:34:31 -0300

    Kotlin Algorithms | Maths | Exponentiation

--- maths/src/main/kotlin/exponentiation/Exponentiation.kt
+++ maths/src/main/kotlin/exponentiation/Exponentiation.kt
@@ -17,4 +17,27 @@ class Exponentiation {
             else -> recursive(base, exponent - 1) * base
         }
     }
+
+    /**
+     * Performs exponentiation of a base to a power using an optimized recursive approach.
+     *
+     * This method calculates `base` raised to the power of `exponent` recursively using the fast exponentiation method.
+     *
+     * @param base The base value.
+     * @param exponent The exponent value.
+     * @return The result of `base` raised to the power of `exponent`.
+     */
+    fun fastRecursive(base: Int, exponent: Int): Int {
+        return when (exponent) {
+            0 -> 1
+            1 -> base
+            else -> {
+                val resultOnHalfExponent = fastRecursive(base, exponent / 2)
+                if ((exponent % 2) == 0)
+                    resultOnHalfExponent * resultOnHalfExponent
+                else
+                    resultOnHalfExponent * resultOnHalfExponent * base
+            }
+        }
+    }
 }
--- maths/src/test/kotlin/exponentiation/ExponentiationUnitTest.kt
+++ maths/src/test/kotlin/exponentiation/ExponentiationUnitTest.kt
@@ -28,4 +28,36 @@ class ExponentiationUnitTest {
         assertEquals(0, solution.recursive(0, 5))
         assertEquals(0, solution.recursive(0, 100))
     }
+
+    @Test
+    fun `Test fast exponentiation`() {
+        // Positive exponent
+        assertEquals(1, solution.fastRecursive(2, 0))
+        assertEquals(2, solution.fastRecursive(2, 1))
+        assertEquals(4, solution.fastRecursive(2, 2))
+        assertEquals(8, solution.fastRecursive(2, 3))
+        assertEquals(16, solution.fastRecursive(2, 4))
+
+        // Negative base
+        assertEquals(1, solution.fastRecursive(-2, 0))
+        assertEquals(-2, solution.fastRecursive(-2, 1))
+        assertEquals(4, solution.fastRecursive(-2, 2))
+        assertEquals(-8, solution.fastRecursive(-2, 3))
+        assertEquals(16, solution.fastRecursive(-2, 4))
+
+        // Larger numbers
+        assertEquals(1024, solution.fastRecursive(2, 10))
+        assertEquals(59049, solution.fastRecursive(3, 10))
+        assertEquals(1000000000, solution.fastRecursive(10, 9))
+
+        // One as base
+        assertEquals(1, solution.fastRecursive(1, 100))
+        assertEquals(1, solution.fastRecursive(1, 0))
+        assertEquals(1, solution.fastRecursive(1, 1))
+
+        // Zero as base
+        assertEquals(0, solution.fastRecursive(0, 1))
+        assertEquals(0, solution.fastRecursive(0, 5))
+        assertEquals(0, solution.fastRecursive(0, 100))
+    }
 }
