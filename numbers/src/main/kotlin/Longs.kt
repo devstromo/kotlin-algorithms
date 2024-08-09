f4b5e4facf2986ef26d83041d9d12f12e6d7290a
alemoles <alemolerio@gmail.com>
2024-08-08 23:26:36 -0300

    Kotlin Algorithms | Numbers | Long | Binary Conversion

--- numbers/src/main/kotlin/Longs.kt
+++ numbers/src/main/kotlin/Longs.kt
@@ -13,3 +13,19 @@ fun toBinaryUsingDivideAndModulusLong(numberToConvert: Long): String {
 
     return builder.reverse().toString().ifEmpty { "0" }
 }
+
+fun toBinaryUsingShiftsAndModulusLong(numberToConvert: Long): String {
+    require(numberToConvert >= 0) { "Method argument cannot be negative. number=$numberToConvert" }
+
+    val builder = StringBuilder()
+    var longNumber = numberToConvert
+    var temp: Long
+
+    while (longNumber > 0) {
+        temp = longNumber
+        longNumber = temp shr 1
+        builder.append(temp % 2)
+    }
+
+    return builder.reverse().toString().ifEmpty { "0" }
+}
--- numbers/src/test/kotlin/LongsKtUnitTest.kt
+++ numbers/src/test/kotlin/LongsKtUnitTest.kt
@@ -28,4 +28,30 @@ class LongsKtUnitTest {
         val result = toBinaryUsingDivideAndModulusLong(255)
         assertEquals("11111111", result, "Binary representation of 255 should be '11111111'")
     }
+
+    @Test
+    fun `Test to binary using shifts and modulus with positive number input`() {
+        val result = toBinaryUsingShiftsAndModulusLong(10)
+        assertEquals("1010", result, "Binary representation of 10 should be '1010'")
+    }
+
+    @Test
+    fun `Test to binary using shifts and modulus with zero input`() {
+        val result = toBinaryUsingShiftsAndModulusLong(0)
+        assertEquals("0", result, "Binary representation of 0 should be '0'")
+    }
+
+    @Test
+    fun `Test to binary using shifts and modulus with negative number input`() {
+        val exception = assertThrows<IllegalArgumentException> {
+            toBinaryUsingShiftsAndModulusLong(-1)
+        }
+        assertEquals("Method argument cannot be negative. number=-1", exception.message)
+    }
+
+    @Test
+    fun `Test to binary using shifts and modulus with large number input`() {
+        val result = toBinaryUsingShiftsAndModulusLong(255)
+        assertEquals("11111111", result, "Binary representation of 255 should be '11111111'")
+    }
 }
