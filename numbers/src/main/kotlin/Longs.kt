5052eed8e648dadf5dfdc23425a08fc67ccc0b80
alemoles <alemolerio@gmail.com>
2024-08-08 23:20:25 -0300

    Kotlin Algorithms | Numbers | Long | Binary Conversion

---
+++ numbers/src/main/kotlin/Longs.kt
@@ -0,0 +1,15 @@
+fun toBinaryUsingDivideAndModulus(numberToConvert: Long): String {
+    require(numberToConvert >= 0) { "Method argument cannot be negative. number=$numberToConvert" }
+
+    val builder = StringBuilder()
+    var longNumber = numberToConvert
+    var temp: Long
+
+    while (longNumber > 0) {
+        temp = longNumber
+        longNumber = temp / 2
+        builder.append(temp % 2)
+    }
+
+    return builder.reverse().toString().ifEmpty { "0" } // Handle the case when the input is 0
+}
