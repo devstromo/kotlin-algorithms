8c17409fcce72b1251173b6a9eb63eea3faab3b2
alemoles <alemolerio@gmail.com>
2024-08-08 23:30:41 -0300

    Kotlin Algorithms | Numbers | Long | Binary Conversion

--- numbers/src/main/kotlin/Longs.kt
+++ numbers/src/main/kotlin/Longs.kt
@@ -1,3 +1,5 @@
+import java.math.BigDecimal
+
 fun toBinaryUsingDivideAndModulusLong(numberToConvert: Long): String {
     require(numberToConvert >= 0) { "Method argument cannot be negative. number=$numberToConvert" }
 
@@ -25,6 +27,24 @@ fun toBinaryUsingShiftsAndModulusLong(numberToConvert: Long): String {
         temp = longNumber
         longNumber = temp shr 1
         builder.append(temp % 2)
+    }
+
+    return builder.reverse().toString().ifEmpty { "0" }
+}
+
+fun toBinaryUsingBigDecimalLong(numberToConvert: Long): String {
+    require(numberToConvert >= 0) { "Method argument cannot be negative. number=$numberToConvert" }
+
+    val builder = StringBuilder()
+    val zero = BigDecimal.ZERO
+    val two = BigDecimal(2)
+    var number = BigDecimal(numberToConvert)
+    var decimals: Array<BigDecimal>
+
+    while (number > zero) {
+        decimals = number.divideAndRemainder(two)
+        number = decimals[0]
+        builder.append(decimals[1].toInt())
     }
 
     return builder.reverse().toString().ifEmpty { "0" }
--- numbers/src/test/kotlin/LongsKtUnitTest.kt
+++ numbers/src/test/kotlin/LongsKtUnitTest.kt
@@ -54,4 +54,30 @@ class LongsKtUnitTest {
         val result = toBinaryUsingShiftsAndModulusLong(255)
         assertEquals("11111111", result, "Binary representation of 255 should be '11111111'")
     }
+
+    @Test
+    fun `Test to binary using BigDecimal with positive number input`() {
+        val result = toBinaryUsingBigDecimalLong(10)
+        assertEquals("1010", result, "Binary representation of 10 should be '1010'")
+    }
+
+    @Test
+    fun `Test to binary using BigDecimal with zero input`() {
+        val result = toBinaryUsingBigDecimalLong(0)
+        assertEquals("0", result, "Binary representation of 0 should be '0'")
+    }
+
+    @Test
+    fun `Test to binary using BigDecimal with negative number input`() {
+        val exception = assertThrows<IllegalArgumentException> {
+            toBinaryUsingBigDecimalLong(-1)
+        }
+        assertEquals("Method argument cannot be negative. number=-1", exception.message)
+    }
+
+    @Test
+    fun `Test to binary using BigDecimal with large number input`() {
+        val result = toBinaryUsingBigDecimalLong(255)
+        assertEquals("11111111", result, "Binary representation of 255 should be '11111111'")
+    }
 }
