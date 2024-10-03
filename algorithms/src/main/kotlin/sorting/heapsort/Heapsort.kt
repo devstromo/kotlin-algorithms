669ba238adc2bd74ba9394218b92c427e0a63ab9
alemoles <alemolerio@gmail.com>
2024-10-02 22:00:49 -0300

    Kotlin Algorithms | Sorting | Heapsort

--- algorithms/src/main/kotlin/sorting/heapsort/Heapsort.kt
+++ algorithms/src/main/kotlin/sorting/heapsort/Heapsort.kt
@@ -1,7 +1,31 @@
 package sorting.heapsort
 
 class Heapsort {
-    fun heapSort(array: IntArray) {
+    /**
+     * Sorts an array of integers in ascending order using the Heap Sort algorithm.
+     *
+     * Heap Sort works by first converting the array into a Max Heap and then repeatedly
+     * moving the largest element (root of the heap) to the end of the array, reducing
+     * the heap size, and re-heapifying the remaining elements.
+     *
+     * This process continues until the array is fully sorted.
+     *
+     * **Stability**: Heap Sort is **not stable**. Stability in sorting means that two elements with the same value
+     * maintain their relative order from the input in the output. In Heap Sort, due to the way elements are
+     * rearranged when the heap is built and re-heapified, elements with the same value might not retain
+     * their original order in the sorted output.
+     *
+     * @param array The array of integers to be sorted.
+     *
+     * Time Complexity:
+     * - Worst-case: O(n log n), where n is the number of elements in the array. Building the heap takes O(n),
+     *   and each removal of the max element takes O(log n), repeated for n elements.
+     * - Average-case: O(n log n), same as the worst case, since the algorithm behaves similarly in all cases.
+     * - Best-case: O(n log n), even in cases where the array is already sorted, heapification still takes log n time.
+     *
+     * Space Complexity: O(1), as Heap Sort is an in-place sorting algorithm and does not require extra space beyond the input array.
+     */
+    fun sort(array: IntArray) {
         val n = array.size
 
         for (i in n / 2 - 1 downTo 0) {
