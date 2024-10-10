c2a04fdf5c0ad78fb6b9602fdc8f695900e6c01e
alemoles <alemolerio@gmail.com>
2024-10-09 22:41:24 -0300

    Kotlin Algorithms | Sequence | Prefix Sum

--- algorithms/src/main/kotlin/sequences/prefix_sum/PrefixSum.kt
+++ algorithms/src/main/kotlin/sequences/prefix_sum/PrefixSum.kt
@@ -17,9 +17,32 @@ class PrefixSum {
 
     fun sumRangeNoAdditionalSpace(nums: IntArray, left: Int, right: Int): Int {
         var sum = 0
-        for (i in left .. right) {
+        for (i in left..right) {
             sum += nums[i]
         }
         return sum
+    }
+
+    fun findMaxLength(nums: IntArray): Int {
+        val n = nums.size
+        val prefixSumIndex = IntArray(2 * n + 1) { -2 }
+        prefixSumIndex[n] = -1
+
+        var maxLength = 0
+        var prefixSum = 0
+
+        for (i in nums.indices) {
+            prefixSum += if (nums[i] == 0) -1 else 1
+
+            val adjustedIndex = prefixSum + n
+
+            if (prefixSumIndex[adjustedIndex] >= -1) {
+                maxLength = maxOf(maxLength, i - prefixSumIndex[adjustedIndex])
+            } else {
+                prefixSumIndex[adjustedIndex] = i
+            }
+        }
+
+        return maxLength
     }
 }
--- algorithms/src/test/kotlin/sequences/prefix_sum/PrefixSumUnitTest.kt
+++ algorithms/src/test/kotlin/sequences/prefix_sum/PrefixSumUnitTest.kt
@@ -19,4 +19,11 @@ class PrefixSumUnitTest {
         assertEquals(-1, solution.sumRangeNoAdditionalSpace(intArrayOf(-2, 0, 3, -5, 2, -1), 2, 5))
         assertEquals(-3, solution.sumRangeNoAdditionalSpace(intArrayOf(-2, 0, 3, -5, 2, -1), 0, 5))
     }
+
+    @Test
+    fun `Test prefix sum to find max length`() {
+        assertEquals(2, solution.findMaxLength(intArrayOf(0, 1)))
+        assertEquals(2, solution.findMaxLength(intArrayOf(0, 1, 0)))
+        assertEquals(8, solution.findMaxLength(intArrayOf(0, 0, 0, 0, 1, 1, 1, 1)))
+    }
 }
