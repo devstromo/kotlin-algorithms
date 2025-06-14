f49138b5483fd76eaf1d845094b516157944058c
alemoles <alemolerio@gmail.com>
2025-06-13 23:09:18 -0300

    Kotlin Algorithms | DP | Longest Palindromic Subsequence

--- algorithms/src/main/kotlin/dp/palindromic_subsequence/PalindromicSubsequence.kt
+++ algorithms/src/main/kotlin/dp/palindromic_subsequence/PalindromicSubsequence.kt
@@ -37,4 +37,27 @@ class PalindromicSubsequence {
 
         return dp[0][n - 1]
     }
+
+    fun longestPalindromeSubseqLCS(s: String): Int {
+        val reveredString = s.reversed()
+        return lcs(s, reveredString)
+    }
+
+    private fun lcs(text1: String, text2: String): Int {
+        val m = text1.length
+        val n = text2.length
+        val dp = Array(m + 1) { IntArray(n + 1) }
+
+        for (i in 1..m) {
+            for (j in 1..n) {
+                if (text1[i - 1] == text2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+                } else {
+                    dp[i][j] = maxOf(dp[i - 1][j], dp[i][j - 1])
+                }
+            }
+        }
+
+        return dp[m][n]
+    }
 }
--- algorithms/src/test/kotlin/dp/palindromic_subsequence/PalindromicSubsequenceUnitTest.kt
+++ algorithms/src/test/kotlin/dp/palindromic_subsequence/PalindromicSubsequenceUnitTest.kt
@@ -48,4 +48,47 @@ class PalindromicSubsequenceUnitTest {
         assertEquals(expected, solver.longestPalindromeSubseq(input))
     }
 
+
+    @Test
+    fun `Test longestPalindromeSubseqLCS with simple case`() {
+        val input = "bbbab"
+        val expected = 4
+        assertEquals(expected, solver.longestPalindromeSubseqLCS(input))
+    }
+
+    @Test
+    fun `Test longestPalindromeSubseqLCS with full palindrome`() {
+        val input = "racecar"
+        val expected = 7
+        assertEquals(expected, solver.longestPalindromeSubseqLCS(input))
+    }
+
+    @Test
+    fun `Test longestPalindromeSubseqLCS with no repeated characters`() {
+        val input = "abcde"
+        val expected = 1
+        assertEquals(expected, solver.longestPalindromeSubseqLCS(input))
+    }
+
+    @Test
+    fun `Test longestPalindromeSubseqLCS with empty string`() {
+        val input = ""
+        val expected = 0
+        assertEquals(expected, solver.longestPalindromeSubseqLCS(input))
+    }
+
+    @Test
+    fun `Test longestPalindromeSubseqLCS with one character`() {
+        val input = "z"
+        val expected = 1
+        assertEquals(expected, solver.longestPalindromeSubseqLCS(input))
+    }
+
+    @Test
+    fun `Test longestPalindromeSubseqLCS with even length palindrome`() {
+        val input = "abccba"
+        val expected = 6
+        assertEquals(expected, solver.longestPalindromeSubseqLCS(input))
+    }
+
 }
